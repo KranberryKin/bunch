e2a1012e278b2e4abcc12c15fa1746ac4921fce7
KranberryKin <quinnclimbs@gmail.com>
2024-11-24 12:33:48 -0600

    Update HomePage

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,12 +1,18 @@
 import './homepage.css'
 
 const HomePage = () => {
+    const title = "Welcome to Bunch!"
+
+    const content = `I hope you enjoy your stay while using Bunch. Bunch is an static webapp that can help manage Tasks and plans. Bunch in a work in progress, and will be receiving consistent updates thoughout app development.`
+
     return (
-    <div className="body-content home-container">
-        
-            Hello From HomePage
-    </div>
-)
+        <div className="body-content home-container">
+            <h1>{title}</h1>
+            <p className='home-text-content'>
+                {content}
+            </p>
+        </div>
+    )
 }
 
 export default HomePage;
