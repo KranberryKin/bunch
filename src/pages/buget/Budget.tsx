1d21585f3bce4f354c96174af2378137a129f20d
Kranberrykin <quinnclimbs@gmail.com>
2025-11-08 13:21:37 -0600

    Update some styles

--- src/pages/buget/Budget.tsx
+++ src/pages/buget/Budget.tsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import './budget.css'
 import { IBudget } from "../../constants/interfaces/budget";
+import Button from "../../components/button/button.tsx";
 
 const Budget = () => {
     const [budgets, setBudgets] = useState<IBudget[]>([]);
 
+    
+
     return (
         <div className="budget-main-container">
-            <div>
-                Your Budgets 
-                <button>Add</button> 
+            <div className="title-container">
+                <h4>
+                    Your Budgets 
+                </h4>
+                <div>
+                    <Button buttonLabel="Add" clicked={() => {}}/>
+                </div>
             </div>
-            <div>
+            <div className="budget-table">
                 {budgets.map((budget, index) => (
                 <div key={`${budget.name} ${index}`}>{budget.name}</div>
                 ))}
